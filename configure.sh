bc2981e351771816e96bdee9c23445f57151ddc6
wuqianlonggit <110802101+wuqianlonggit@users.noreply.github.com>
2024-12-14 23:27:14 +0800

    Update configure.sh
    711-1

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
-# To change the cuda arch, edit Makefile.am and run ./build.sh
+extracflags="-Ofast -ffinite-loops -ffast-math -D_REENTRANT -falign-functions=64 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector -march=armv8.4-a+crypto+sha2+crc"
 
-extracflags="-O3 -ffinite-loops -ffast-math -mfix-cortex-a53-835769 -D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fno-stack-protector"
-
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mtune=cortex-a53" CFLAGS="-finline-functions -Wl,-hugetlbfs-align -march=armv8-a+crypto -mtune=cortex-a53  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize  $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ CC=clang LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
-
+./configure \
+    CXXFLAGS="-funroll-loops -finline-functions -mllvm -enable-loop-distribute $extracflags -mtune=cortex-x1 -mtune=cortex-a78 -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize" \
+    CFLAGS="-funroll-loops -finline-functions -mllvm -enable-loop-distribute $extracflags -mtune=cortex-x1 -mtune=cortex-a78  -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize" \
+    CXX=clang++ CC=clang \
+    LDFLAGS="-flto -Wl,-hugetlbfs-align"
